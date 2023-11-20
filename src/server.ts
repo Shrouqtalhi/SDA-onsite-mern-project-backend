5f354968b1540dfff5033caabfdeb5abb643d7b3
mohammed <malajaji.97@gmail.com>
2023-11-21 00:49:55 +0300

    model auther and borrow Done :)

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,13 @@ import productsRouter from './routers/products'
 import ordersRouter from './routers/orders'
 import apiErrorHandler from './middlewares/errorHandler'
 import myLogger from './middlewares/logger'
+import authorsRouter from './routers/author'
+import borrowsRouter from './routers/borrow'
 
 config()
 const app = express()
 const PORT = 5050
-const URL = process.env.ATLAS_URL as string
+const URL = process.env.MONGO_URL as string
 
 app.use(myLogger)
 app.use(express.urlencoded({ extended: true }))
@@ -20,6 +22,8 @@ app.use(express.json())
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
 app.use('/api/products', productsRouter)
+app.use('/api/authors', authorsRouter)
+app.use('/api/borrows', borrowsRouter)
 
 app.use(apiErrorHandler)
 
