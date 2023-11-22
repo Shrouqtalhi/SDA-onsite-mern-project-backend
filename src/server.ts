519167c0dccb34b8e930e1fc08dc0a2a1921ab23
Shrouqtalhi <shrouq.talhi@hotmail.com>
2023-11-22 08:58:44 +0300

    Create book schema, connect db

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,20 @@
-import express from 'express'
+import express, { Application } from 'express'
 import mongoose from 'mongoose'
-import { config } from 'dotenv'
 
-import usersRouter from './routers/users'
-import productsRouter from './routers/products'
-import ordersRouter from './routers/orders'
 import apiErrorHandler from './middlewares/errorHandler'
 import myLogger from './middlewares/logger'
+import bookRouter from './routers/bookRoutes'
+import { dev } from './config'
 
-config()
-const app = express()
-const PORT = 5050
-const URL = process.env.ATLAS_URL as string
+const app: Application = express()
+const PORT = dev.app.PORT
+const URL = dev.db.ATLAS_URL as string
 
 app.use(myLogger)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/api/users', usersRouter)
-app.use('/api/orders', ordersRouter)
-app.use('/api/products', productsRouter)
+app.use('/api/books', bookRouter)
 
 app.use(apiErrorHandler)
 
@@ -33,5 +28,5 @@ mongoose
   })
 
 app.listen(PORT, () => {
-  console.log('Server running http://localhost:' + PORT)
+  console.log(`Server running http://localhost:${PORT}`)
 })
