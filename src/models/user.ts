54e7b61e5dc1bb30037c410a7dea30b88102b83e
Emtenan5 <98597006+Emtenan5@users.noreply.github.com>
2023-11-22 13:37:22 +0300

    creatr user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,43 @@
 import mongoose from 'mongoose'
 
-
+export type UserDocument = Document & {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  role: string
+}
 const userSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
     required: true,
   },
-  // relation between order and user should be many orders to one user
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  role: {
+    type: String,
+    required: true,
+  },
+  // relation between borrow and user should be many borrows to one user
   // here's 1to1 just for the demo
-  order: {
+  borrow: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Order',
+    ref: 'Borrow',
   },
 })
 
-export default mongoose.model('Client', userSchema)
+export default mongoose.model('User', userSchema)
