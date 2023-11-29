63a492207429cde6689ac9be53d60ad42981b4f4
Emtenan5 <98597006+Emtenan5@users.noreply.github.com>
2023-11-29 15:05:55 +0300

    update email

--- src/util/email.ts
+++ src/util/email.ts
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAILER_USER,
     pass: process.env.MAILER_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 })
 
 export async function sendActivationEmail(userEmail: string, activationToken: string) {
